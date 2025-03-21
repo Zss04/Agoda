650ff809751a7551599231b6c1f4e88a18cdc728
Zaki Sabir Shaikh <zaki.sabir@arbisoft.com>
2025-03-21 13:09:49 +0500

    docker runner

--- tests/test_search_info.py
+++ tests/test_search_info.py
@@ -4,7 +4,6 @@ from tabulate import tabulate
 
 
 @pytest.mark.asyncio
-@pytest.mark.agoda
 async def test_verify_search_results(page_tuple):
     # create an instance of trip options class
     page, get_url = page_tuple
@@ -16,7 +15,6 @@ async def test_verify_search_results(page_tuple):
 
 
 @pytest.mark.asyncio
-@pytest.mark.agoda
 async def test_flight_data(page_tuple):
     page, get_url = page_tuple   # recieves tuple from page fixture
     trip = test_flightInfo(page)    # creates instance  of flightinfo class
