9d2a6f8541266c8d57a617660295b629b095a64d
Zaki Sabir Shaikh <zaki.sabir@arbisoft.com>
2025-03-24 17:35:18 +0500

    added parallel execution

--- conftest.py
+++ conftest.py
@@ -79,9 +79,9 @@ async def browser(request):
         if browser_name == "chromium":
             browser = await p.chromium.launch(headless=True)
         elif browser_name == "firefox":
-            browser = await p.firefox.launch(headless=False)
+            browser = await p.firefox.launch(headless=True)
         elif browser_name == "webkit":
-            browser = await p.webkit.launch(headless=False)
+            browser = await p.webkit.launch(headless=True)
         else:
             pytest.fail(f"Unsupported browser: {browser_name}")
         yield browser
