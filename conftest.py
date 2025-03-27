9ac1cccbcff40052db3191a04ab44bb9a276b1f4
Zaki Sabir Shaikh <zaki.sabir@arbisoft.com>
2025-03-27 22:33:19 +0500

    Added package.json scripts

--- conftest.py
+++ conftest.py
@@ -89,11 +89,11 @@ async def browser(request):
 
     async with async_playwright() as p:
         if browser_name == "chromium":
-            browser = await p.chromium.launch(headless=False)
+            browser = await p.chromium.launch(headless=True)
         elif browser_name == "firefox":
-            browser = await p.firefox.launch(headless=False)
+            browser = await p.firefox.launch(headless=True)
         elif browser_name == "webkit":
-            browser = await p.webkit.launch(headless=False)
+            browser = await p.webkit.launch(headless=True)
         else:
             pytest.fail(f"Unsupported browser: {browser_name}")
         yield browser
